8bb3628b5aa8e167b680124cd8d23e8beb4faa15
Md Sohaib <mdsohaibuddin@gmail.com>
2021-08-11 23:32:17 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('Attrition.urls')),
-    path('', include('attrition_show.urls')),
 ]
